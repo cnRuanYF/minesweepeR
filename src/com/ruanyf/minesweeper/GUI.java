f917bf87c9728895e74c4fae5a07ff454c3ec4dc
YaoFeng Ruan <yfruan@outlook.com>
2018-04-04 19:28:52 +0800

    GUI更新 (新标题界面、快速排雷)
    + 标题界面窗口自适应
    + 提供3种默认难度
    + 实现鼠标中键快速排雷

--- src/com/ruanyf/minesweeper/GUI.java
+++ src/com/ruanyf/minesweeper/GUI.java
@@ -1,19 +1,25 @@
 package com.ruanyf.minesweeper;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-import javax.swing.BorderFactory;
 import java.awt.Font;
 import java.awt.Insets;
-import java.awt.Color;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /**
  * 扫雷GUI
@@ -25,65 +31,19 @@ public class GUI extends JFrame {
 	public static final String TITLE = "minesweepeR";
 	public static final String SYMBOL_MINE = "💀";
 	public static final String SYMBOL_FLAG = "🚩";
-	
+
 	// 数字颜色 (1~8)
 	public static final Color[] COLORS_NUM = { new Color(64, 80, 192), new Color(16, 102, 0), new Color(176, 0, 0), new Color(0, 0, 128), new Color(128, 0, 0), new Color(0, 128, 128), new Color(160, 0, 0), new Color(176, 0, 0) };
 
 	private JPanel titlePane;
 	private JPanel gamePane;
+	private JPanel menuPane;
 	private JLabel lblTitle;
-	private JButton btnStartEasy;
-	private JButton btnStartNormal;
-	private JButton btnStartHard;
 
 	private JLabel[][] tiles;
 	private JButton[][] covers;
 
 	private Core core = new Core();
-
-	// 开始游戏按钮事件
-	private ActionListener btnActionListener = new ActionListener() {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			Object src = e.getSource();
-			if (src == btnStartEasy) {
-				startGame(Core.SIZE_EASY_ROW, Core.SIZE_EASY_COL, Core.SIZE_EASY_MINE);
-			} else if (src == btnStartNormal) {
-				startGame(Core.SIZE_NORMAL_ROW, Core.SIZE_NORMAL_COL, Core.SIZE_NORMAL_MINE);
-			} else if (src == btnStartHard) {
-				startGame(Core.SIZE_HARD_ROW, Core.SIZE_HARD_COL, Core.SIZE_HARD_MINE);
-			}
-		}
-	};
-
-	// 扫雷按钮事件
-	private MouseAdapter sweepActionListener = new MouseAdapter() {
-
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			Object obj = e.getSource();
-			int btn = e.getButton();
-			// 遍历所有按钮
-			for (int row = 0; row < tiles.length; row++) {
-				for (int col = 0; col < tiles[0].length; col++) {
-					// 匹配发生点击事件的按钮
-					if (obj == covers[row][col]) {
-						if (btn == MouseEvent.BUTTON1) {
-							open(row, col); // 左键翻开
-						} else if (btn == MouseEvent.BUTTON3) {
-							flag(row, col); // 右键标记
-						}
-						row = tiles.length; // 使条件失效以跳出循环
-						break;
-					}
-				}
-			}
-
-			super.mouseClicked(e);
-		}
-
-	};
 
 	/**
 	 * Create the frame.
@@ -91,7 +51,13 @@ public class GUI extends JFrame {
 	public GUI() {
 		// 设置窗口 (Frame)
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(200, 200, 640, 480);
+		setMinimumSize(new Dimension(640, 480));
+		setSize(640, 480);
+		
+		// 窗口居中
+		int scrWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+		int scrHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+		setLocation(scrWidth / 2 - getWidth() / 2, scrHeight / 2 - getHeight() / 2);
 
 		// 显示标题界面
 		showTitle();
@@ -106,21 +72,78 @@ public class GUI extends JFrame {
 
 		// 设置容器 (Panel)
 		titlePane = new JPanel();
-		titlePane.setLayout(null);
-		setContentPane(titlePane);
+		titlePane.setBackground(Color.WHITE);
+		titlePane.setLayout(new BoxLayout(titlePane, BoxLayout.Y_AXIS));
 
 		// 标题
 		lblTitle = new JLabel(TITLE, JLabel.CENTER);
-		titlePane.add(lblTitle);
-		lblTitle.setBounds(0, 100, 640, 100);
-		lblTitle.setFont(new Font("微软雅黑", Font.PLAIN, 32));
+		lblTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblTitle.setPreferredSize(new Dimension(0, 128));
+		lblTitle.setFont(new Font(null, Font.ITALIC, 48));
 
-		// 开始按钮
-		btnStartEasy = new JButton();
-		titlePane.add(btnStartEasy);
-		btnStartEasy.setBounds(240, 240, 160, 50);
-		btnStartEasy.addActionListener(btnActionListener);
-		btnStartEasy.setText("Start");
+		// 按钮的容器
+		menuPane = new JPanel();
+		menuPane.setOpaque(false);
+		menuPane.setLayout(new BoxLayout(menuPane, BoxLayout.X_AXIS));
+
+		// 开始按钮的属性定义
+		Font btnFont = new Font(null, Font.PLAIN, 24);
+		String[] btnTexts = new String[] { "Easy", "Normal", "Hard" };
+		Color[] btnColors = new Color[] { new Color(0, 192, 0), new Color(0, 160, 255), new Color(255, 64, 0) };
+		JButton[] btns = new JButton[btnTexts.length];
+
+		// 开始按钮的事件
+		ActionListener btnActionListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String cmd = e.getActionCommand();
+				if (btnTexts[0].equals(cmd)) {
+					startGame(Core.SIZE_EASY_ROW, Core.SIZE_EASY_COL, Core.SIZE_EASY_MINE);
+				} else if (btnTexts[1].equals(cmd)) {
+					startGame(Core.SIZE_NORMAL_ROW, Core.SIZE_NORMAL_COL, Core.SIZE_NORMAL_MINE);
+				} else if (btnTexts[2].equals(cmd)) {
+					startGame(Core.SIZE_HARD_ROW, Core.SIZE_HARD_COL, Core.SIZE_HARD_MINE);
+				}
+			}
+		};
+
+		// 设定按钮
+		for (int i = 0; i < btns.length; i++) {
+			// 实例化
+			btns[i] = new JButton();
+
+			// 设置尺寸
+			btns[i].setMinimumSize(new Dimension(128, 128));
+			btns[i].setMaximumSize(new Dimension(128, 128));
+			btns[i].setPreferredSize(new Dimension(128, 128));
+
+			// 前景(文字)颜色
+			btns[i].setBackground(btnColors[i]);
+			btns[i].setForeground(Color.WHITE);
+			btns[i].setFont(btnFont);
+			btns[i].setText(btnTexts[i]);
+
+			// 事件监听
+			btns[i].addActionListener(btnActionListener);
+
+			// 添加按钮和占位元素到面板
+			menuPane.add(btns[i]);
+			menuPane.add(Box.createHorizontalStrut(32)); // 占位元素
+		}
+
+		// 移除最后一个多余的占位元素 (Strut)
+		menuPane.remove(menuPane.getComponentCount() - 1);
+
+		// 添加组件到TitlePane
+		titlePane.add(Box.createVerticalGlue()); // 自适应占位元素
+		titlePane.add(lblTitle);
+		titlePane.add(menuPane);
+		titlePane.add(Box.createVerticalGlue());
+
+		setContentPane(titlePane);
+		// setContentPane之后有时界面不更新，需要更改窗口大小来强制重绘 (BUG?)
+		setSize(getWidth() + 1, getHeight() + 1);
+		setSize(getWidth() - 1, getHeight() - 1);
 	}
 
 	/**
@@ -135,7 +158,7 @@ public class GUI extends JFrame {
 	 */
 	private void startGame(int rowSize, int colSize, int mineSize) {
 		// UI尺寸
-		int tileSize = 48;
+		int tileSize = 32;
 		int paddingHorizontal = 64;
 		int paddingVertical = 64;
 
@@ -146,6 +169,38 @@ public class GUI extends JFrame {
 		// 初始化核心
 		core.init(rowSize, colSize, mineSize);
 
+		// 扫雷按钮事件
+		MouseAdapter sweepActionListener = new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Object obj = e.getSource();
+				int btn = e.getButton();
+
+				// 遍历所有按钮
+				boolean flag = true;
+				for (int row = 0; flag && row < tiles.length; row++) {
+					for (int col = 0; flag && col < tiles[0].length; col++) {
+
+						// 匹配发生点击事件的按钮
+						if (obj == covers[row][col] && btn == MouseEvent.BUTTON1) {
+							open(row, col); // 左键翻开
+							flag = false;
+						} else if (obj == covers[row][col] && btn == MouseEvent.BUTTON3) {
+							flag(row, col); // 右键标记
+							flag = false;
+						} else if (obj == tiles[row][col] && btn == MouseEvent.BUTTON2) {
+							sweep(row, col); // 中键快速排雷
+							flag = false;
+						}
+					}
+				}
+
+				super.mouseClicked(e);
+			}
+
+		};
+
 		// 根据尺寸创建数组
 		tiles = new JLabel[rowSize][colSize];
 		covers = new JButton[rowSize][colSize];
@@ -153,7 +208,6 @@ public class GUI extends JFrame {
 		// 创建容器 (Panel)
 		gamePane = new JPanel();
 		gamePane.setLayout(null);
-		setContentPane(gamePane);
 
 		// 创建控件
 		for (int r = 0; r < rowSize; r++) {
@@ -181,60 +235,14 @@ public class GUI extends JFrame {
 				covers[r][c].setBorder(BorderFactory.createLineBorder(Color.GRAY));
 				covers[r][c].setFont(new Font(null, Font.BOLD, tileSize * 3 / 4));
 				covers[r][c].addMouseListener(sweepActionListener);
+				tiles[r][c].addMouseListener(sweepActionListener);
 			}
 		}
-	}
 
-	/**
-	 * 翻开格子
-	 * 
-	 * @param row
-	 *            行索引 (从0起算)
-	 * @param col
-	 *            列索引 (从0起算)
-	 */
-	private void open(int row, int col) {
-
-		// 没有标记且未翻开才可翻开
-		if (core.status[row][col] == Core.STAT_COVER) {
-			core.open(row, col);
-
-			setTitle(TITLE + " ( Remain: " + core.remain + " , Flag: " + core.flags + " )");
-
-			// 刷新所有格子状态
-			for (int r = 0; r < tiles.length; r++) {
-				for (int c = 0; c < tiles[0].length; c++) {
-					boolean isOpened = core.status[r][c] == Core.STAT_OPEN;
-					tiles[r][c].setVisible(isOpened);
-					covers[r][c].setVisible(!isOpened);
-
-					// 游戏结束则标出雷的位置
-					if (core.isGameover) {
-						if (core.isMine(r, c)&&core.status[r][c] == Core.STAT_COVER ) {
-							// 未标记的雷
-							covers[r][c].setText(SYMBOL_MINE);
-						} else if (core.isMine(r, c)&&core.status[r][c] == Core.STAT_FLAG ){
-							// 标记正确的雷
-							covers[r][c].setForeground(new Color(64, 128, 96));
-						} else if(!core.isMine(r, c)&&core.status[r][c] == Core.STAT_FLAG){
-							// 标记错误的雷
-							covers[r][c].setForeground(Color.RED);
-						}
-					}
-				}
-			}
-
-			// 游戏结束判定
-			if (core.isGameover) {
-				tiles[row][col].setOpaque(true);
-				tiles[row][col].setBackground(Color.RED);
-				JOptionPane.showMessageDialog(null, "  Gameover ...", TITLE, JOptionPane.ERROR_MESSAGE);
-				showTitle();
-			} else if (core.isWin) {
-				JOptionPane.showMessageDialog(null, "  You Win !", TITLE, JOptionPane.PLAIN_MESSAGE);
-				showTitle();
-			}
-		}
+		setContentPane(gamePane);
+		// setContentPane之后有时界面不更新，需要更改窗口大小来强制重绘 (BUG?)
+		setSize(getWidth() + 1, getHeight() + 1);
+		setSize(getWidth() - 1, getHeight() - 1);
 	}
 
 	/**
@@ -259,6 +267,92 @@ public class GUI extends JFrame {
 			} else {
 				covers[row][col].setText("");
 			}
+		}
+	}
+
+	/**
+	 * 翻开格子
+	 * 
+	 * @param row
+	 *            行索引 (从0起算)
+	 * @param col
+	 *            列索引 (从0起算)
+	 */
+	private void open(int row, int col) {
+
+		// 没有标记且未翻开才可翻开
+		if (core.status[row][col] == Core.STAT_COVER) {
+			core.open(row, col);
+
+			setTitle(TITLE + " ( Remain: " + core.remain + " , Flag: " + core.flags + " )");
+
+			// 刷新所有格子状态
+			refreshStatus();
+		}
+	}
+
+	/**
+	 * 快速排雷 (若周边雷数与周边标记数一致，则翻开周边未标记的格子)
+	 * 
+	 * @param row
+	 *            行索引 (从0起算)
+	 * @param col
+	 *            列索引 (从0起算)
+	 */
+	private void sweep(int row, int col) {
+
+		// 已经翻开才可以对周边进行快速排雷
+		if (core.status[row][col] == Core.STAT_OPEN) {
+			core.sweep(row, col);
+
+			setTitle(TITLE + " ( Remain: " + core.remain + " , Flag: " + core.flags + " )");
+
+			// 刷新所有格子状态
+			refreshStatus();
+		}
+	}
+
+	/**
+	 * 刷新所有格子并更新游戏状态
+	 * 
+	 * @param row
+	 *            行索引 (从0起算)
+	 * @param col
+	 *            列索引 (从0起算)
+	 */
+	private void refreshStatus() {
+
+		for (int r = 0; r < tiles.length; r++) {
+			for (int c = 0; c < tiles[0].length; c++) {
+				boolean isOpened = core.status[r][c] == Core.STAT_OPEN;
+				tiles[r][c].setVisible(isOpened);
+				covers[r][c].setVisible(!isOpened);
+
+				// 游戏结束则标出雷的位置
+				if (core.isGameover) {
+					if (core.isMine(r, c) && core.status[r][c] == Core.STAT_COVER) {
+						// 未标记的雷
+						covers[r][c].setText(SYMBOL_MINE);
+					} else if (core.isMine(r, c) && core.status[r][c] == Core.STAT_FLAG) {
+						// 标记正确的雷
+						covers[r][c].setForeground(new Color(64, 128, 96));
+					} else if (!core.isMine(r, c) && core.status[r][c] == Core.STAT_FLAG) {
+						// 标记错误的雷
+						covers[r][c].setForeground(Color.RED);
+					}
+				}
+			}
+		}
+
+		// 游戏结束判定
+		if (core.isGameover) {
+			tiles[core.deadRow][core.deadCol].setOpaque(true);
+			tiles[core.deadRow][core.deadCol].setBackground(Color.RED);
+			JOptionPane.showMessageDialog(null, "  Gameover ...", TITLE, JOptionPane.ERROR_MESSAGE);
+			showTitle();
+		} else if (core.isWin) {
+			JOptionPane.showMessageDialog(null, "  You Win !", TITLE, JOptionPane.PLAIN_MESSAGE);
+			showTitle();
 		}
 	}
 
